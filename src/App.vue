b4a6c0df0145bc55801be25b6cd1a08970e9b578
Mick <codebymick@gmail.com>
2019-04-19 14:53:27 +0200

    rebase to github

--- src/App.vue
+++ src/App.vue
@@ -87,7 +87,7 @@ export default {
     list-style: none;
     padding: 0;
     margin: 0;
-    background-color: rgba(255,255,255,.8);
+    background-color: white;
     z-index:999;
     }
 
@@ -107,16 +107,11 @@ export default {
     white-space: nowrap;
     border:2px solid transparent;
     border-radius:15px;
-
-    border-radius: 15px;
-    border:2px solid #2c3e50;
-    margin-right: 2px;
     }
 
   ul li a:hover {
     background: #2c3e50;
-    color:rgba(255,255,255,.8)
-  }
+    color:whitesmoke}
 
   li:hover > ul {
     display: block;
